3e78175f2a8702f6d4c25850200c477eb363c6f9
FraRussotto <russottofrancesco1@gmail.com>
2023-10-09 15:59:02 +0200

    [edit]

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import Title from './components/Title.vue';
+  export default {
+    name: 'App',
+    components: {
+      Title
+    },
+    data(){
+      return{
+        titolo: "La mia prima App con Vite",
+      }
+    }
+  }
+
+</script>
+
+<template>
+  <Title></Title>
+  <h1></h1>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'Title',
+  data(){
+    return  {
+      text: 'La mia prima App con Vite!',
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+
+<style scoped>
+  h1{
+    text-align: center;
+    margin-top: 30px;
+    font-size: 3rem; 
+  }
+</style>
